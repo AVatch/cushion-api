e8c0f6573651e2c015ecef908405f743e006ab48
Adrian Vatchinsky <avatchinsky@outlook.com>
2016-04-04 15:50:34 -0400

    location models

--- cushion/locations/models.py
+++ cushion/locations/models.py
@@ -1,5 +1,28 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import logging
+LOGGER = logging.getLogger(__name__)
 
-# Create your models here.
+
+class LocationCategory(models.Model):
+    name = models.CharField(max_length=140)
+    
+    time_created = models.DateTimeField(auto_now_add=True)
+    time_modified = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return '%s' % ( self.name, )
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=140, blank=True)
+    category = models.ManyToManyField(LocationCategory)
+    lat = models.FloatField()
+    lng = models.FloatField()
+
+    time_created = models.DateTimeField(auto_now_add=True)
+    time_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '%s <%s,%s>' % ( self.name, str(self.lat), str(self.lng) )
