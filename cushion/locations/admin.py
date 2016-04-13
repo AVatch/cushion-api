e187fc0773e14cd2ea54aae71e742c780baceee1
Adrian Vatchinsky <avatchinsky@outlook.com>
2016-04-13 11:09:05 -0400

    added coordinates to admin

--- cushion/locations/admin.py
+++ cushion/locations/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
-from .models import Location, LocationCategory
+from .models import Location, LocationCategory, Coordinates
+
+class CoordinatesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'latitude', 'longitude', 'account')
+    list_display_links = ('id',)
+admin.site.register(Coordinates, CoordinatesAdmin)
+
 
 class LocationCategoryAdmin(admin.ModelAdmin):
     pass
